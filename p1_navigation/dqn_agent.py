0ddddb224b1b2118936fbec171046945217c5cfc
brianx0215 <brianxu0215@gmail.com>
2021-03-09 14:18:50 +0000

    Implement agent learning from visual input

--- p1_navigation/dqn_agent.py
+++ p1_navigation/dqn_agent.py
@@ -17,16 +17,52 @@ UPDATE_EVERY = 4        # how often to update the network
 use_cuda = torch.cuda.is_available()
 
 class DQN(nn.Module):
-    #Customized DQN class for Udacity deep learning project 1
-    def __init__(self, state_size, action_size, seed, use_pixel_input=False):
+    #Customized DQN class for Udacity deep reinforcement learning project 1
+    def __init__(self, state_size, action_size, seed):
         super(DQN, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, action_size)
-        self.drop = nn.Dropout(p = 0.2)
 
     def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+def conv(in_channels, out_channels, kernel_size, stride = 1, padding = 1, batch_norm = True):
+    layers = []
+    conv_layer = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias = False)
+    layers.append(conv_layer)
+    
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(out_channels))
+        
+    return nn.Sequential(*layers)
+
+class CNNDQN(nn.Module):
+    #Customized CNN and DQN class for Udacity deep reinforcement learning project 1
+    #The class is referenced from the projects in Udacity deep learning course.
+    def __init__(self, state_size, action_size, seed):
+        super(CNNDQN, self).__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self.conv1 = conv(3, 4, 3, batch_norm = False)
+        self.conv2 = conv(4, 8, 3)
+        self.conv3 = conv(8, 16, 3)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.fc1 = nn.Linear(800, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_size)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 800)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -35,16 +71,20 @@ class DQN(nn.Module):
 class Agent():
     # The agent interacts with and learns from the banana environment.
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, action_size, visual_input, seed):
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(seed)
 
         # we use Double DQN for the agent
-        self.dqn_local = DQN(state_size, action_size, seed)
-        self.dqn_target = DQN(state_size, action_size, seed)
+        if visual_input:
+            self.dqn_local = CNNDQN(state_size, action_size, seed)
+            self.dqn_target = CNNDQN(state_size, action_size, seed)
+        else:
+            self.dqn_local = DQN(state_size, action_size, seed)
+            self.dqn_target = DQN(state_size, action_size, seed)
+            
         self.optimizer = optim.Adam(self.dqn_local.parameters(), lr=LR)
-
         if use_cuda:
             dqn_local, dqn_target = dqn_local.cuda(), dqn_target.cuda()
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
@@ -80,16 +120,16 @@ class Agent():
         #Update value parameters using given batch of experience tuples.
 
         states, actions, rewards, next_states, dones = experiences
-
         self.dqn_local.train()
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.dqn_target(next_states).detach().max(1)[0].unsqueeze(1)
-        # Compute Q targets for current states 
+        Q_targets_next = self.dqn_target(next_states).detach().max(1)[0]
+
+        # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
         # Get expected Q values from local model
-        Q_expected = self.dqn_local(states).gather(1, actions)
-
+        #                       64, 4              64
+        Q_expected = self.dqn_local(states).gather(1, actions.unsqueeze(1)).squeeze(1)
         loss = F.mse_loss(Q_expected, Q_targets)
         self.optimizer.zero_grad()
         loss.backward()
@@ -132,11 +172,11 @@ class ReplayBuffer:
         #Randomly sample a batch of experiences from memory.
         experiences = random.sample(self.memory, k=self.batch_size)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float()
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long()
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float()
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float()
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float()
+        states = torch.FloatTensor([e.state for e in experiences if e is not None])
+        actions = torch.LongTensor([e.action for e in experiences if e is not None])
+        rewards = torch.FloatTensor([e.reward for e in experiences if e is not None])
+        next_states = torch.FloatTensor([e.next_state for e in experiences if e is not None])
+        dones = torch.FloatTensor([e.done for e in experiences if e is not None])
   
         if use_cuda:
             states, actions, rewards, next_states, dones = states.cuda(), actions.cuda(), rewards.cuda(), next_states.cuda(), dones.cuda()
